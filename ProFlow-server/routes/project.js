7f160a7999567078cdd66279f5107497ed9351f0
nisha-bugalia <nishakr0605@gmail.com>
2025-06-26 18:20:31 +0530

    added some api's easy ones

--- ProFlow-server/routes/project.js
+++ ProFlow-server/routes/project.js
@@ -85,4 +85,51 @@ router.get("/get-projects", verifyIdentity, async (req, res) => {
     console.log(err);
   }
 });
+
+//api to get the project by id
+router.get("/:id", verifyIdentity, async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("🔍 Fetching project ID:", id);
+
+    const project = await Project.findOne({ _id: id });
+
+    if (!project) {
+      console.log("✅ Project found:", project);
+
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    res.status(200).json({ project });
+  } catch (error) {
+    console.log("❌ Error fetching project:", error.message);
+
+    res.status(500).json({
+      message:
+        process.env.NODE_ENV === "development"
+          ? error.message
+          : "Internal Server Error",
+    });
+  }
+});
+//api to delete the project
+router.delete("/:id", verifyIdentity, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findByIdAndDelete(id);
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+    res.status(200).json({ message: "Project deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting project:", error);
+    res.status(500).json({
+      message:
+        process.env.NODE_ENV === "development"
+          ? error.message
+          : "Internal Server Error",
+    });
+  }
+});
+
 module.exports = router;
